73d62bd7d0448c148892822ac4c5940293b18cc3
Raul M. Laredo Soria <raul.laredo@fundacion-jala.org>
2019-10-02 21:09:36 -0400

    Update

---
+++ test/com/mypro/hello/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package com.mypro.hello;
+import com.mypro.hello.HelloWorld;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test public void testHelloWorldMessage() {
+        HelloWorld classUnderTest = new HelloWorld();
+        assertNotNull("Hello", "Hello");
+    }
+    @Test public void testHelloWorldMessageIsPrinted() {
+        HelloWorld classUnderTest = new HelloWorld();
+        assertNotNull("Hello", "Hello");
+    }
+    @Test public void testHelloWorldMessageIsNotNull() {
+        HelloWorld classUnderTest = new HelloWorld();
+        assertEquals("Hello", "Hello");
+    }        
+}
+
